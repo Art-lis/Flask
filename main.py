e5a8cec4a0e31aec57f684fda28c550977793bef
artur.liszewski <artur.liszewski@onwelo.com>
2021-11-08 13:53:48 +0100

    Lab2

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from random import randint
 
 app = Flask(__name__)
 
+version = '1.2'
+
 @app.route('/get-item',endpoint='get-item' ,methods=['GET'])
 def main():
     return str(randint(0, 1000))
@@ -13,4 +15,5 @@ def author():
     return 'Artur Liszewski'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print(version)
+    app.run(debug=True, host='0.0.0.0')
