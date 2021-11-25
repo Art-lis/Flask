3b2bb6764bad8f1ac1d87753c844b3ca7ccb4ac9
artur.liszewski <artur.liszewski@onwelo.com>
2021-11-25 09:47:30 +0100

    envVar - odd even all

--- main.py
+++ main.py
@@ -11,14 +11,42 @@ outputFile = 'output.txt'
 
 @app.route('/get-item',endpoint='get-item' ,methods=['GET'])
 def main():
-    randnum = str(randint(0, 1000))
+    envVar = os.environ.get('NUMBER_TYPE')
+    choice = False
+
+    while choice == False:
+        randnum = randint(0, 1000)
+        if envVar == 'even':
+            if isEven(randnum):
+                choice = True
+        elif choice == 'odd':
+            if isOdd(randnum):
+                choice = True
+        else:
+            choice = True
+
+
+    randstr = str(randnum)
     if not os.path.exists(archivePath):
         print('no volume attached')
     else:
         f = open(archivePath+'/'+outputFile, 'a')
-        f.write(randnum+'\n')
+        f.write(randstr+'\n')
         f.close()
-    return randnum
+
+    return randstr
+
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+def isOdd(num):
+    if num % 2 != 0:
+        return True
+    else:
+        return False
 
 
 @app.route('/author', endpoint='author', methods=['GET'])
